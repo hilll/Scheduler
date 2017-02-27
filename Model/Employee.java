d420f07d1770e37a6a4f1ada4b19960c0994f18d
hilll <levihill@Levis-MacBook-Air.local>
2017-02-26 17:25:40 -0700

    Getting Dummyinfo setup

--- Model/Employee.java
+++ Model/Employee.java
@@ -1,4 +1,15 @@
 
 public class Employee {
+	
+	int empID;
+	String empName;
+	int prefHours;
+	
+	public Employee(int id, String name, int hours){
+		this.empID = id;
+		this.empName = name;
+		this.prefHours=hours;
+		
+	}
 
 }
--- Model/Main.java
+++ Model/Main.java
@@ -1,4 +1,18 @@
 
 public class Main {
+	
+	public static void main(String args[]){
+		
+		/* Intialize Dummy Employees*/
+		Employee[]	staff = new Employee[10];
+		for(int i = 0; i < staff.length; ++i){
+			String name = "Staff(" + i + ")";
+			staff[i] = new Employee(i, name, 40);
+		}
+		
+		
+		
+	}
+	
 
 }

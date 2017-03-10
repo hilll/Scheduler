62c7f32738b410432587b3e6dc5afc9bcd594810
Sarina White <sarinarw@gmail.com>
2017-03-10 12:58:56 -0700

    added LocalConfig.java to manually define project path to get serialization vis JSP to work

---
+++ TheGreatestScheduleEverScheduled/src/resources/LocalConfig.java
@@ -0,0 +1,13 @@
+package resources;
+
+/*
+ * hacks get bitches
+ */
+public class LocalConfig {
+
+	// this is my project path, replace it with YOURS
+	// easiest way to get path is to right click or whatever on the project in the Project Explorer
+	// go to Properties at the very bottom and copy it from there, paste it here and add the "/" at the end
+	public static String projectDir = "/Users/apple/Documents/Education/Spring2017/"
+			+ "CSC436/Scheduler/TheGreatestScheduleEverScheduled/"; // make sure to add ending "/"!!
+}
